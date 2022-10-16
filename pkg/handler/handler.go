8027d437df1a39a23dbe0d86a1d582f455efd39a
guff192 <kochkons1@ya.ru>
2022-10-16 21:40:49 +0400

    Added handler function for placement request

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -9,8 +9,13 @@ func NewHandler() *Handler {
 	return &Handler{}
 }
 
-func (h *Handler) InitRoutes() *gin.Engine {
+func (h Handler) InitRoutes() *gin.Engine {
 	router := gin.New()
+
+	placements := router.Group("/placemnets")
+	{
+		placements.POST("request/", h.placementRequest)
+	}
 
 	return router
 }
