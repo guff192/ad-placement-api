66524968ee0108fd7c28a67f8dd36f4b450456f2
guff192 <kochkons1@ya.ru>
2022-10-20 18:06:30 +0400

    Changed types, added convert function

--- tile.go
+++ tile.go
@@ -1,7 +1,23 @@
 package placement
 
+import (
+	"math"
+)
+
 type Tile struct {
 	Id    uint    `json:"id"`
 	Width uint    `json:"width"`
-	Ratio float32 `json:"ratio"`
+	Ratio float64 `json:"ratio"`
+}
+
+func (t *Tile) ToImpRequest() *ImpRequest {
+	id := t.Id
+	minwidth := t.Width
+	minheight := math.Floor(float64(t.Width) * t.Ratio)
+
+	return &ImpRequest{
+		Id:        id,
+		Minwidth:  minwidth,
+		Minheight: uint(minheight),
+	}
 }
