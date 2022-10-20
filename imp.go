3a6055d88a0f896114799a49139b6081e2da85cb
guff192 <kochkons1@ya.ru>
2022-10-21 00:43:47 +0400

    Added response model, fixed field type

--- imp.go
+++ imp.go
@@ -1,16 +1,24 @@
 package placement
 
 type Imp struct {
-	Id     uint   `json:"id"`
-	Width  uint   `json:"width"`
-	Height uint   `json:"height"`
-	Title  string `json:"title"`
-	URL    string `json:"url"`
-	Price  string `json:"price"`
+	Id     uint    `json:"id"`
+	Width  uint    `json:"width"`
+	Height uint    `json:"height"`
+	Title  string  `json:"title"`
+	URL    string  `json:"url"`
+	Price  float64 `json:"price"`
 }
 
 type ImpRequest struct {
 	Id        uint `json:"id"`
 	Minwidth  uint `json:"minwidth"`
 	Minheight uint `json:"minheight"`
+}
+
+type ImpResponse struct {
+	Id     uint   `json:"id"`
+	Width  uint   `json:"width"`
+	Height uint   `json:"height"`
+	Title  string `json:"title"`
+	URL    string `json:"url"`
 }
